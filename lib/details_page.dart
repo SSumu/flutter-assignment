8529c28b3bc357f8b8fd2b51a50049af4b5dae5a
SSumu <sadeepalsumudu@gmail.com>
2024-06-19 17:17:55 +0530

    Created the Details Page

---
+++ lib/details_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_assignment/product_model.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class DetailsPage extends StatelessWidget {
+  final Product product;
+
+  const DetailsPage({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            CarouselSlider(
+              options: CarouselOptions(height: 400.0),
+              items: product.images.map((image) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Image.network(
+                      image,
+                      fit: BoxFit.cover,
+                      width: MediaQuery.of(context).size.width,
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Price: \$${product.price.toStringAsFixed(2)}',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            Text('Ratings:${product.rating}'),
+            const SizedBox(height: 16),
+            Text(product.description),
+          ])),
+    );
+  }
+}
